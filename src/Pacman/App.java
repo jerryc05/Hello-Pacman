fa296c27f07cb1f4aaf82c9df6016af663bc4a24
Ziyan Jerry Chen <jerryc443@gmail.com>
2019-11-22 11:13:12 -0500

    init push

---
+++ src/Pacman/App.java
@@ -0,0 +1,28 @@
+package Pacman;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This is the  main class where your Pacman game will start.
+ * The main method of this application calls the App constructor. You
+ * will need to fill in the constructor to instantiate your game.
+ * <p>
+ * Class comments here...
+ */
+
+public class App extends Application {
+
+  @Override
+  public void start(Stage stage) {
+    // Create top-level object, set up the scene, and show the stage here.
+  }
+
+  /*
+   * Here is the mainline! No need to change this.
+   */
+  public static void main(String[] argv) {
+    // launch is a method inherited from Application
+    launch(argv);
+  }
+}
